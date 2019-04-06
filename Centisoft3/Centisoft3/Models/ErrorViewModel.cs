9c8d2f2571cc1a609a931473119f5dc042767f11
Vít Vašák <vitvasak@outlook.com>
2019-04-06 15:35:29 +0200

    Project created

---
+++ Centisoft3/Centisoft3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Centisoft3.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
